613e5022e226de5970c98a3fdd585a5b97adb5a0
Joel Vernon <joel.l.vernon@wmich.edu>
2022-10-18 13:54:42 -0400

    Various improvements

--- main.py
+++ main.py
@@ -1,46 +1,70 @@
-import program_module as pm
+# Import dependencies
+import p4_module as pm
 
-file_prefix = input("Enter a file prefix: ")
-old_file_name = f"{file_prefix}_old.txt"
-new_file_name = f"{file_prefix}_new.txt"
+# Import constants
+import constants as const
 
-try:
-    with open(old_file_name, "r") as old_file, open(new_file_name, "w") as new_file:
 
-        loop_sentinel = "999999"
-        file_line = old_file.readline().strip("\n")
+# Define main function
+def main():
+    # Get file name from user.
+    file_prefix = input("Enter a file prefix: ")
 
-        while file_line != loop_sentinel:
-            account_number = pm.get_number(file_line)
-            account_balance = pm.get_balance(file_line)
-            customer_name = pm.get_name(file_line)
-            print(f"Verifying input: {account_number} {account_balance} {customer_name}")
+    # Assign variables for original file name and for new file name
+    old_file_name = f"{file_prefix}_old.txt"
+    new_file_name = f"{file_prefix}_new.txt"
 
-            while True:
-                transaction_code = pm.get_transaction_code()
-                if transaction_code == "a":
-                    new_line = f"{pm.file_entry_string(account_number, account_balance, customer_name)}"
-                    print(f"New balance: {new_line}")
-                    new_file.write(f"{new_line}\n")
-                    break
-
-                elif transaction_code == "c":
-                    if pm.account_is_closable(account_balance):
-                        break
-
-                elif transaction_code in ("w", "d"):
-                    if transaction_code == "w":
-                        transaction_amount = pm.get_transaction_amount("withdrawal")
-                        account_balance = pm.process_transaction(transaction_code, account_balance, transaction_amount)
-                    else:
-                        transaction_amount = pm.get_transaction_amount("deposit")
-                        account_balance = pm.process_transaction(transaction_code, account_balance, transaction_amount)
-
-                else:
-                    print(f"Entered an invalid command {transaction_code}")
-
+    # Try/Except to catch FileNotFound Error when attempting to read a file that does not exist.
+    try:
+        # Open original file and new file
+        with open(old_file_name, "r") as old_file, open(new_file_name, "w") as new_file:
+            end_of_file = const.SENTINEL
             file_line = old_file.readline().strip("\n")
-        new_file.write("999999")
 
-except FileNotFoundError:
-    print(f"The file name {old_file_name} does not exist.")
+            # while loop that runs until End of File is reached
+            while file_line != end_of_file:
+                account_number = pm.get_number(file_line)
+                account_balance = pm.get_balance(file_line)
+                customer_name = pm.get_name(file_line)
+                print(f"Verifying input: {account_number} {account_balance} {customer_name}")
+
+                # while loop that runs until the user is done preforming transactions on customer's account
+                while True:
+                    transaction_code = pm.get_transaction_code()
+                    # Write updated customer data to new file after all transactions have been preformed and breaks loop
+                    if transaction_code == "a":
+                        new_line = f"{pm.file_entry_string(account_number, account_balance, customer_name)}"
+                        print(f"New balance: {new_line}")
+                        new_file.write(f"{new_line}\n")
+                        break
+                    elif transaction_code == "c":
+                        # Breaks while loop without writing customer data to new file only if account balance = 0.00
+                        if pm.account_is_closable(account_balance):
+                            break
+                    elif transaction_code in ("w", "d"):
+                        # withdraw money from account if transaction_code is "w"
+                        if transaction_code == "w":
+                            transaction_amount = pm.get_transaction_amount("withdrawal")
+                            account_balance = pm.process_transaction(transaction_code, account_balance,
+                                                                     transaction_amount)
+                        # otherwise deposit money if transaction code is "d"
+                        else:
+                            transaction_amount = pm.get_transaction_amount("deposit")
+                            account_balance = pm.process_transaction(transaction_code, account_balance,
+                                                                     transaction_amount)
+                    # Print an error message if invalid transaction code is entered.
+                    else:
+                        print(f"Entered an invalid command {transaction_code}")
+                # Read next line of old file
+                file_line = old_file.readline().strip("\n")
+            # Print sentinel at end of new file
+            new_file.write("999999")
+
+    except FileNotFoundError:
+        print(f"The file name {old_file_name} does not exist.")
+    else:
+        print(f"\n{new_file_name} created successfully.")
+
+
+# Call main
+main()
--- p4_module.py
+++ p4_module.py
@@ -1,20 +1,21 @@
-FIRST_ACCOUNT_NUMBER, LAST_ACCOUNT_NUMBER = 100_000, 999_998
-ACCOUNT_NUMBER_SENTINEL = 999_999
-MINIMUM_ACCOUNT_BALANCE, MAXIMUM_ACCOUNT_BALANCE = 0.00, 9_999_999.99
+# Import file of constant values
+import constants as const
 
 
+# Functions for parsing user data from file lines
 def get_number(line: str) -> str:
-    return line[:6]
+    return line[:const.END_OF_ACCOUNT_NUMBER]
 
 
 def get_balance(line: str) -> float:
-    return float(line[7:17].lstrip(" "))
+    return float(line[const.START_OF_ACCOUNT_BALANCE:const.END_OF_ACCOUNT_BALANCE].lstrip(" "))
 
 
 def get_name(line: str) -> str:
-    return line[18:]
+    return line[const.START_OF_CUSTOMER_NAME:]
 
 
+# Functions for processing account transactions
 def process_transaction(transaction_type: str, balance: float, transaction_amount: float) -> float:
     if transaction_type == "w":
         return balance - transaction_amount
@@ -33,18 +34,6 @@ def get_transaction_amount(text: str) -> float:
             return float(transaction_amount)
 
 
-def is_float(num: str) -> bool:
-    try:
-        float(num)
-        return True
-    except ValueError:
-        return False
-
-
-def file_entry_string(account_number: str, balance: float, name: str) -> str:
-    return f"{account_number} {balance:10.2f} {name}"
-
-
 def get_transaction_code() -> str:
     while True:
         transaction_code = input("Enter a command (a,c,d,w): ")
@@ -55,6 +44,7 @@ def get_transaction_code() -> str:
     return transaction_code.lower()
 
 
+# Helper functions for transaction processing
 def account_is_closable(balance):
     if balance == 0:
         print("Account is closed")
@@ -62,3 +52,15 @@ def account_is_closable(balance):
     print("Account not closed because money is still in it")
     return False
 
+
+def is_float(num: str) -> bool:
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
+
+# Function to format strings to write to new file
+def file_entry_string(account_number: str, balance: float, name: str) -> str:
+    return f"{account_number} {balance:10.2f} {name}"
